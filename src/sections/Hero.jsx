15e1eb429d437452722018ae6c03d4f222fc1583
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-21 11:30:11 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -15,7 +15,7 @@ function Hero() {
           </div>
         </div>
 
-        <a href="https://solscan.io/token/AiBdJXqhDVjPvhDZcykjMwdhJaNKuqE4rSbamMqq31Nq" target="_blank" className=" font-groot-two text-xs sm:text-xl text-center my-3 ">CONTRACT: AiBdJXqhDVjPvhDZcykjMwdhJaNKuqE4rSbamMqq31Nq</a>
+        <a href="https://solscan.io/token/AiBdJXqhDVjPvhDZcykjMwdhJaNKuqE4rSbamMqq31Nq" target="_blank" className=" font-groot-two text-xs sm:text-xl text-center my-3 flex items-center justify-center ">CONTRACT: AiBdJXqhDVjPvhDZcykjMwdhJaNKuqE4rSbamMqq31Nq</a>
 
         <div className=" flex justify-center gap-4 items-center">
           <a href="/" className="font-groot-one text-2xl uppercase text-black bg-[#ffffffbe] border-2 border-[#000000] py-2 px-6 rounded-full">
