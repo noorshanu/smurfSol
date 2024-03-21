542bae991dbac353a76a86d315707db9fca61062
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-21 18:32:07 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,19 +10,37 @@ function Hero() {
             </h1>
 
             <div className=" w-full">
-              <img src="images/hero2.png" alt="" className=" h-auto  mx-auto floating" />
+              <img
+                src="images/hero2.png"
+                alt=""
+                className=" h-auto  mx-auto floating"
+              />
             </div>
           </div>
         </div>
 
-        <a href="https://solscan.io/token/AiBdJXqhDVjPvhDZcykjMwdhJaNKuqE4rSbamMqq31Nq" target="_blank" className=" font-groot-two text-xs sm:text-xl text-center my-3 flex items-center justify-center ">CONTRACT: AiBdJXqhDVjPvhDZcykjMwdhJaNKuqE4rSbamMqq31Nq</a>
+        <a
+          href="https://solscan.io/token/AiBdJXqhDVjPvhDZcykjMwdhJaNKuqE4rSbamMqq31Nq"
+          target="_blank"
+          className=" font-groot-two text-xs sm:text-xl text-center my-3 flex items-center justify-center "
+        >
+          CONTRACT: AiBdJXqhDVjPvhDZcykjMwdhJaNKuqE4rSbamMqq31Nq
+        </a>
 
         <div className=" flex justify-center gap-4 items-center">
-          <a href="https://www.dextools.io/app/en/solana/pair-explorer/BSCXAbGA2XreZ9NM4a5sVup2HGTWC4VcJgAuQ3z8TpmM?t=1711006149740" target="_blank" className="font-groot-one text-2xl uppercase text-black bg-[#ffffffbe] border-2 border-[#000000] py-2 px-6 rounded-full">
+          <a
+            href="https://www.dextools.io/app/en/solana/pair-explorer/BSCXAbGA2XreZ9NM4a5sVup2HGTWC4VcJgAuQ3z8TpmM?t=1711006149740"
+            target="_blank"
+            className="font-groot-one text-2xl uppercase text-black bg-[#ffffffbe] border-2 border-[#000000] py-2 px-6 rounded-full"
+          >
             DexTools
           </a>
-          <a href="https://t.me/smurfsol" target="_blank" className="font-groot-one text-2xl uppercase text-black bg-[#ffffffbe] border-2 border-[#000000] py-2 px-6 rounded-full">
-       Telegram
+          <a
+            href="https://t.me/smurfsol"
+            target="_blank"
+            className="font-groot-one text-2xl uppercase text-black bg-[#ffffffbe] border-2 border-[#000000] py-2 px-6 rounded-full"
+          >
+            Telegram
           </a>
         </div>
       </div>
