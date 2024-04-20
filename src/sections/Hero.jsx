74d745eb4428dc91d6b2e0905e900c1f23414edf
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-04-20 22:24:31 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -20,16 +20,16 @@ function Hero() {
         </div>
 
         <a
-          href="https://solscan.io/token/AiBdJXqhDVjPvhDZcykjMwdhJaNKuqE4rSbamMqq31Nq"
+          href="https://solscan.io/token/3z4qzzoLBJpeBHofPb5s88desjdp52gg8HqnzYdvop7b"
           target="_blank"
           className=" font-groot-two text-xs sm:text-xl text-center my-3 flex items-center justify-center "
         >
-          CONTRACT: AiBdJXqhDVjPvhDZcykjMwdhJaNKuqE4rSbamMqq31Nq
+          CONTRACT: 3z4qzzoLBJpeBHofPb5s88desjdp52gg8HqnzYdvop7b
         </a>
 
         <div className=" flex justify-center gap-4 items-center">
           <a
-            href="https://www.dextools.io/app/en/solana/pair-explorer/BSCXAbGA2XreZ9NM4a5sVup2HGTWC4VcJgAuQ3z8TpmM?t=1711006149740"
+            href="https://www.dextools.io/app/en/solana/pair-explorer/BN57dZ9rbcZo3jRrRggztaK6qCap9MBnu6pvMwEzEBL4?t=1713631592155"
             target="_blank"
             className="font-groot-one text-2xl uppercase text-black bg-[#ffffffbe] border-2 border-[#000000] py-2 px-6 rounded-full"
           >
